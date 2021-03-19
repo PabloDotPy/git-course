633cac138b1fb35310cf6abc2dfbb8950b09221c
PabloDotPy <kirnicki0907@gmail.com>
2021-03-19 10:08:05 +0100

    Eslint setup

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,20 @@
 const gulp = require("gulp");
 const babel = require("gulp-babel");
+const eslint = require("gulp-eslint");
 
 // gulp dependencies will be here
 gulp.task('default', function () {
+    //Eslint
+    gulp.src(["es6/**/*.js", "public/es6/**/*.js"])
+        .pipe(eslint())
+        .pipe(eslint.format());
     //code for Node
     gulp.src("es6/**/*.js")
-    .pipe(babel())
-    .pipe(gulp.dest("dist"));
+        .pipe(babel())
+        .pipe(gulp.dest("dist"));
     //code for browser
     gulp.src("public/es6/**/*.js")
-    .pipe(babel())
-    .pipe(gulp.dest("public/dist"));
-    
+        .pipe(babel())
+        .pipe(gulp.dest("public/dist"));
+
 });
